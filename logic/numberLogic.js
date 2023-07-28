32a0c441b104efff9e1992fdfda2c5caff0bc8bd
dhruvbansal <bansaldhruv03@gmail.com>
2023-07-28 15:40:35 +0530

    five

--- logic/numberLogic.js
+++ logic/numberLogic.js
@@ -3,20 +3,15 @@ class NumberLogic {
         this.queryString = queryString;
       }
       filter() {
-        // 1A) FILTERING
-        const queryObj = { ...this.queryString };
-        const excludeFields = ['url'];
-    
-        excludeFields.forEach((el) => delete queryObj[el]);
-    
-        // 1B) ADVANCED FILTERING
-        let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replaceAll(
-        //   /\b(gte|gt|lte|lt)\b/g,
-        //   (match) => `$${match}`
-        // );
+        // 1A) FILTERING'
+        let url;
+        if(this.queryString.url){
+           url = this.queryString.url;
+        }
+        console.log(url);
         return this;
         console.log(queryStr);
       }
+      
 }
 module.exports = NumberLogic;
