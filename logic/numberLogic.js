2b000f42227120c54f5dd5cd1ef20953869310d2
dhruvbansal <bansaldhruv03@gmail.com>
2023-07-28 15:58:55 +0530

    siz

--- logic/numberLogic.js
+++ logic/numberLogic.js
@@ -8,10 +8,15 @@ class NumberLogic {
         if(this.queryString.url){
            url = this.queryString.url;
         }
-        console.log(url);
-        return this;
-        console.log(queryStr);
-      }
+        console.log(url[0]);
+        let querystr = fetch(url[0],{
+          headers:{
+            Authentication: 'Authorization',
+          }
+        })
+        querystr.then(res=> res.json()).then(value=>console.log(value))
       
+      }
+
 }
 module.exports = NumberLogic;
