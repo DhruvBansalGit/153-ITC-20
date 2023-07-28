34dd7710c51327e79a916c0ff2304dd59358a25a
dhruvbansal <bansaldhruv03@gmail.com>
2023-07-28 16:09:53 +0530

    seven

--- logic/numberLogic.js
+++ logic/numberLogic.js
@@ -8,13 +8,25 @@ class NumberLogic {
         if(this.queryString.url){
            url = this.queryString.url;
         }
-        console.log(url[0]);
-        let querystr = fetch(url[0],{
-          headers:{
-            Authentication: 'Authorization',
-          }
-        })
-        querystr.then(res=> res.json()).then(value=>console.log(value))
+        // console.log(url[0]);
+        let value ={
+          numbers:[]
+        };
+        for(let i = 0;i< url.length;i++){
+          let querystr = fetch(url[i],{
+            headers:{
+              Authentication: 'Authorization',
+            }
+          })
+          querystr.then(res=> res.json()).then(val=> console.log(JSON.stringify(val.numbers)) )
+        }
+        console.log(value.numbers)
+        // let querystr = fetch(url[0],{
+        //   headers:{
+        //     Authentication: 'Authorization',
+        //   }
+        // })
+        // querystr.then(res=> res.json()).then(value=>console.log(value))
       
       }
 
